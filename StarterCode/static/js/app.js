cd1675c2d20b7fbb8ba35683a57509d5cb781d7f
ggiudice98 <giancarlogiudice@live.com>
2024-01-09 16:10:12 -0500

    final

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,7 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 function init() {
-
+    let dropdownmenu = d3.select("#selDataset");
     //Retrieve Json data
         d3.json(url).then(function(Data) {
             console.log(Data);
